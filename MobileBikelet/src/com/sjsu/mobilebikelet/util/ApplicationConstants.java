a6f4fb1e8186a69522d5873fdbc334cd1de4254e
nikitham <nikitha.mamidi@gmail.com>
2013-03-22 00:01:29 -0700

    pointing to cloundfoundry

--- MobileBikelet/src/com/sjsu/mobilebikelet/util/ApplicationConstants.java
+++ MobileBikelet/src/com/sjsu/mobilebikelet/util/ApplicationConstants.java
@@ -4,31 +4,31 @@ public class ApplicationConstants {
 
 	public static String USER_PREF = "USER_PREF";
 
-	public static String APPLICATION_HOST = "http://192.168.1.111:8080";
+	public static String APPLICATION_HOST = "http://bikelet.cloudfoundry.com/";
 	
 	public static String AUTHENTICATION_JSON_URL = APPLICATION_HOST
-			+ "/bikelet/identity/authenticate.json";
+			+ "identity/authenticate.json";
 	
 	public static String GET_STATIONS_BY_PROGRAM_URL = APPLICATION_HOST
-			+ "/bikelet/stations/listStationByProgram.json";
+			+ "stations/listStationByProgram.json";
 	
 	public static String GET_LAST_TRANSACTION_URL = APPLICATION_HOST
-			+ "/bikelet/renttransactions/viewtransaction.json";
+			+ "renttransactions/viewtransaction.json";
 	
 	public static String GET_BIKES_BY_STATION_URL = APPLICATION_HOST
-			+ "/bikelet/stations/listBikeByStation.json";
+			+ "stations/listBikeByStation.json";
 
 	public static String CHECKOUT_URL = APPLICATION_HOST
-			+ "/bikelet/renttransactions/checkoutBike.json";
+			+ "renttransactions/checkoutBike.json";
 	
 	public static String CHECKIN_URL = APPLICATION_HOST
-			+ "/bikelet/renttransactions/checkin";
+			+ "renttransactions/checkin";
 
 	public static String GET_TRANSACTION_URL = APPLICATION_HOST
-			+ "/bikelet/renttransactions/gettransaction.json";
+			+ "renttransactions/gettransaction.json";
 	
 	public static String CHECK_STATIONFULL_URL = APPLICATION_HOST
-			+ "/bikelet/stations/checkstationfull.json";
+			+ "stations/checkstationfull.json";
 	
 	public static String DATE_FORMAT = "MM/dd/yyyy";
 	
