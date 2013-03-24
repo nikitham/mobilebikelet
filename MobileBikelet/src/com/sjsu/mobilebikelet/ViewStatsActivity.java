c9843d2c8eff64f71cf61325bbc3bcad7217da1c
nikitham <nikitha.mamidi@gmail.com>
2013-03-24 00:18:58 -0700

    added ok button to go to home page in view statistics page

--- MobileBikelet/src/com/sjsu/mobilebikelet/ViewStatsActivity.java
+++ MobileBikelet/src/com/sjsu/mobilebikelet/ViewStatsActivity.java
@@ -1,8 +1,11 @@
 package com.sjsu.mobilebikelet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ViewStatsActivity extends Activity {
@@ -18,5 +21,15 @@ public class ViewStatsActivity extends Activity {
 		caloriesTextView.setText(getIntent().getExtras().get("caloriesBurned").toString());
 		co2Saved.setText(getIntent().getExtras().get("co2Saved").toString());
 		
+		Button b = (Button) findViewById(R.id.gohome);
+		b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(ViewStatsActivity.this, HomeScreenActivity.class);
+				startActivity(i);
+			}
+		});
 	}
 }
